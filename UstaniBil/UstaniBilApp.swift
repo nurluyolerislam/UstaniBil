0e0e1454d2632b0cb96031c5308f11f86bae3050
nurluyolerislam <nurluyolerislam@gmail.com>
2022-10-21 18:39:14 +0300

    Initial Commit

---
+++ UstaniBil/UstaniBilApp.swift
@@ -0,0 +1,17 @@
+//
+//  UstaniBilApp.swift
+//  UstaniBil
+//
+//  Created by Erislam Nurluyol on 21.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct UstaniBilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
